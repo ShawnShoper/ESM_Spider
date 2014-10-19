b91987213ca46ff05c0f08463968ca850bfdc62c
Shoper <xiehao3692@vip.qq.com>
2014-10-20 01:26:46 +0800

    add log4j

--- src/org/shoper/spider/ui/MainStage.java
+++ src/org/shoper/spider/ui/MainStage.java
@@ -1,4 +1,18 @@
 package org.shoper.spider.ui;
 
-public class MainStage {
+import org.apache.log4j.Logger;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainStage extends Application {
+	private static Logger logger = Logger.getLogger(MainStage.class);
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.show();
+	}
+	public static void main(String[] args) {
+		
+	}
 }
